47a697d9ee8aa809d31fc3baf6680a4720bac0f4
Тила Кирилл <tilakirill98@gmail.com>
2022-02-12 18:58:12 +0200

    Initial Commit

---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by Кирилл Тила on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
